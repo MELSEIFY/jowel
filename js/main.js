a8c14f70a71ccb767b872da477c2100c0e234561
madimodicom <madimodicom2@gmail.com>
2020-09-11 18:30:56 +0200

    Footer Section has been added to origin repository on github and font-awesome has been updated to the newest version.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$("#lastSection input, #textArea").click(function () {	
+    $(this).css("border-bottom", "1px red solid");	
+  })	
+
+$("#lastSection input, #textArea").blur(function () {	
+    $(this).css("border-bottom", "1px #ced4da solid");	
+});	

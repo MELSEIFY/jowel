2212ce3502ecdfd4d427aa5869c7ad495ffa2999
madimodicom <madimodicom2@gmail.com>
2020-09-11 17:50:43 +0200

    deleted my footer

--- footer/js/index.js
+++
@@ -1,9 +0,0 @@
-$("#lastSection input, #textArea").click(function () {
-    $(this).css("border-bottom", "1px red solid");
-  })
-
-$("#lastSection input, #textArea").blur(function () {
-    $(this).css("border-bottom", "1px #ced4da solid");
-});
-
-

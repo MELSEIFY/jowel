96972f1d266c43c4376d08435528114d3aa1ec7b
mohamedsalah94 <gsalah1001@gmail.com>
2020-09-13 19:50:01 +0200

    Salah Update

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ $("#lastSection input, #textArea").click(function () {
 $("#lastSection input, #textArea").blur(function () {	
     $(this).css("border-bottom", "1px #ced4da solid");	
 });	
+
+
+$("a[href^='#']").click(function(){
+
+  let aHerf = $(this).attr("href");
+
+  let sectionOffset = $(aHerf).offset().top;
+
+  $("html,body").animate( {scrollTop:sectionOffset} , 2000);
+
+});
+
+

a04af2e62a43ca3abadee96d6dc524d452af061a
MhmoudEzat1225 <Mahmoud.ezat199212@gmail.com>
2020-09-14 02:04:37 +0200

    Ezat's portfolio and upBtn

--- js/main.js
+++ js/main.js
@@ -1,30 +1,23 @@
-// essam function
+$("#lastSection input, #textArea").click(function () {	
+    $(this).css("border-bottom", "1px red solid");	
+  })	
 
-$("#lastSection input, #textArea").click(function () {
-  $(this).css("border-bottom", "1px red solid");
-})
+$("#lastSection input, #textArea").blur(function () {	
+    $(this).css("border-bottom", "1px #ced4da solid");	
+});	
 
-$("#lastSection input, #textArea").blur(function () {
-  $(this).css("border-bottom", "1px #ced4da solid");
-});
 
-//salah function
-$("a[href^='#']").click(function () {
+$("a[href^='#']").click(function(){
+
   let aHerf = $(this).attr("href");
-  let sectionOffset = $(aHerf).offset().top - 70;
-  $("html,body").animate({ scrollTop: sectionOffset }, 1000);
+
+  let sectionOffset = $(aHerf).offset().top-70;
+
+  $("html,body").animate( {scrollTop:sectionOffset} , 2000);
+
 });
 
-// mohamed sami funcion
-$("a").click(function () {
-  $(this).addClass('active').parent().siblings().find("a").removeClass('active')
-})
 
-$(window).scroll(function () {
-
-  $('section').each(function () {
-
-<<<<<<< HEAD
 
 // ====== Mahmoud Ezat section ======== 
 
@@ -64,13 +57,3 @@ $(".swipUp").click(function(){
 // ====== Mahmoud Ezat section ======== 
 
 
-=======
-    if ($(window).scrollTop()+125 >= $(this).offset().top)
-    {
-      let secId ="'"+"#"+$(this).attr('id')+"'";
-      $('nav li a').removeClass('active');
-      $(`nav li a[href=${secId}`).addClass('active');
-    }
-  })
-})
->>>>>>> e53e9f70ec03599d26b191618dc4161119831843
